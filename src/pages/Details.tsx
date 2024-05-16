506e6678bf9b2f8766beacbe69423ad5d618685f
anazirjonov <anazirjonov10@gmail.com>
2024-05-16 13:14:50 +0500

    finished

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import img from "../assets/img.png";
+
 interface PhoneType {
   id: string;
   name: string;
